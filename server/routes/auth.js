3a0e87bafcffeb5ca64855173b75f7d108d562b9
Nephrain <96636511+Nephrain@users.noreply.github.com>
2024-01-17 12:35:42 -0500

    Delete server directory

--- server/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const passport = require("passport");
-const router = express.Router();
-
-router.post("/login", passport.authenticate("local"), (req, res) => {
-  res.send(200);
-});
-  
-module.exports = router;
